de5672a095a5b5c4e41162f2e1b99fa2d2bfc540
Carlos Silva <cd160204@gmail.com>
2026-04-30 18:57:26 -0300

    chore: cria estrutura básica de projeto Spring Boot

---
+++ src/main/java/com/carlossilvadev/desafio_tecnico_backend_itau/DesafioBackendUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.carlossilvadev.desafio_tecnico_backend_itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendUrlShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlossilvadev/desafio_tecnico_backend_itau/DesafioBackendUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlossilvadev.desafio_tecnico_backend_itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
