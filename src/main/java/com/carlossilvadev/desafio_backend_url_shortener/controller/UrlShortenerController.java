c55fb274eca7804f0c87b61fa2a17b894e0fa539
Carlos Silva <cd160204@gmail.com>
2026-05-05 19:55:31 -0300

    feat(controller): cria UrlShortenerController e método shortenUrl
    este commit cria a classe UrlShortenerController e o método shortenUrl, o qual recebe a requisição de encurtamento de URL, consome o retorno do método shortenUrl de UrlShortenerService para retornar a URL encurtada ao cliente HTTP

---
+++ src/main/java/com/carlossilvadev/desafio_backend_url_shortener/controller/UrlShortenerController.java
@@ -0,0 +1,32 @@
+package com.carlossilvadev.desafio_backend_url_shortener.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carlossilvadev.desafio_backend_url_shortener.dto.UrlRequestDTO;
+import com.carlossilvadev.desafio_backend_url_shortener.dto.UrlResponseDTO;
+import com.carlossilvadev.desafio_backend_url_shortener.service.UrlShortenerService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+
+@RestController
+public class UrlShortenerController {
+	private final UrlShortenerService service;
+	
+	public UrlShortenerController(UrlShortenerService service) {
+		this.service = service;
+	}
+	
+	@PostMapping("/shorten-url")
+	public ResponseEntity<UrlResponseDTO> shortenUrl(@RequestBody @Valid UrlRequestDTO request, HttpServletRequest servletRequest) {
+		var originalUrl = service.shortenUrl(request).url(); // encurta URL e armazena o campo do DTO
+		
+		var redirectUrl = servletRequest.getRequestURL().toString().replace("shorten-url", originalUrl); // constrói a URL com host e porta + URL curta no caminho URI
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(new UrlResponseDTO(redirectUrl));
+	}
+}
