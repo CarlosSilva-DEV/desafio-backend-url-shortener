dcc39247bd245011f09941d0a0826bf717662e3a
Carlos Silva <cd160204@gmail.com>
2026-05-08 22:15:18 -0300

    chore: renomea pacote de testes do projeto para desafio_backend_url_shortener
    assim como corrigido no commit 0cff6df, este commit corrige a referência do pacote da aplicação em DesafioBackendUrlShortenetApplicationTests

--- src/test/java/com/carlossilvadev/desafio_backend_url_shortener/DesafioBackendUrlShortenerApplicationTests.java
+++ src/test/java/com/carlossilvadev/desafio_backend_url_shortener/DesafioBackendUrlShortenerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.carlossilvadev.desafio_tecnico_backend_itau;
+package com.carlossilvadev.desafio_backend_url_shortener;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
