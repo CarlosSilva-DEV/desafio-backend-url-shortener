119b51f49eef867fbe61e146aab173326a3a18f3
Carlos Silva <cd160204@gmail.com>
2026-05-21 12:44:34 -0300

    test(repository): cria classe UrlRepositoryTest e cria método shouldSaveAndFindByShortenedUrl
    este commit cria a classe UrlRepositoryTest para realizar testes de integração relacionados à camada de repository. o método shouldSaveAndFindByShortenedUrl simula um cenário onde uma entidade Url é persistida e recuperada com sucesso através do método findByShortenedUrl de UrlRepository

---
+++ src/test/java/com/carlossilvadev/desafio_backend_url_shortener/repository/UrlRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.carlossilvadev.desafio_backend_url_shortener.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.redis.test.autoconfigure.DataRedisTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.carlossilvadev.desafio_backend_url_shortener.config.RedisConfig;
+import com.carlossilvadev.desafio_backend_url_shortener.model.Url;
+
+@DataRedisTest
+@Testcontainers
+@Import(RedisConfig.class)
+@ActiveProfiles("test")
+public class UrlRepositoryTest {
+	
+	@Container // cria um container do Redis para testes de integração
+	@ServiceConnection @SuppressWarnings("resource")
+	static final GenericContainer<?> REDIS_CONTAINER = new GenericContainer<>("redis:8.2-alpine").withExposedPorts(6379);
+	
+	@Autowired
+	private UrlRepository repository;
+	
+	@AfterEach // ao final de cada teste, os dados são removidos do Redis, garantindo isolamento entre os testes
+	void tearDown() {
+		repository.deleteAll();
+	}
+	
+	@Test
+	@DisplayName("Deve salvar e recuperar uma entidade Url através da chave shortenedUrl")
+	void shouldSaveAndFindByShortenedUrl() {
+		// ARRANGE
+		Url url = new Url("aBc123", "https://google.com");
+		
+		// ACT
+		repository.save(url);
+		Optional<Url> result = repository.findByShortenedUrl(url.getShortenedUrl());
+		
+		// ASSERT
+		assertTrue(result.isPresent());
+		assertEquals(url.getShortenedUrl(), result.get().getShortenedUrl());
+		assertEquals(url.getOriginalUrl(), result.get().getOriginalUrl());
+	}
+}
